b21e9144efc3d7db08ad88d95d543d0841b5eed8
Денис Заречнев <d.zarech@gmail.com>
2023-10-11 12:35:05 +0300

    added task2

---
+++ task2/task.py
@@ -0,0 +1,33 @@
+def task(edges_csv: str):
+    edges = edges_csv.split('\n')
+    matrix = []
+
+    for edge in edges:
+        sup, inf = map(int, edge.split(','))
+        sup -= 1
+        inf -= 1
+        while sup > len(matrix) - 1 or inf > len(matrix) - 1:
+            matrix.append([0, 0, 0, 0, 0])
+
+        matrix[sup][0] += 1
+        matrix[inf][1] += 1
+
+    for edge in edges:
+        sup, inf = map(int, edge.split(','))
+        sup -= 1
+        inf -= 1
+        matrix[sup][2] += matrix[inf][0]
+        matrix[inf][3] += matrix[sup][1]
+        matrix[inf][4] += matrix[sup][0] - 1
+
+    return matrix
+
+# 1
+#  \
+#   2
+#  /  \
+#  3    6
+#  |  \
+#  4    5
+
+print(task("1,2\n2,3\n2,6\n3,4\n3,5"))
